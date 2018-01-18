f6aa9273ceb3f5c2708ce31f2527dcadf0b781dd
Alika Larsen <alika.larsen@gmail.com>
2018-01-18 11:39:10 -0800

    Publish and deploy chart (#6)
    * feat: adding recommended chart labels
    
    * feat: adding chart publish and deploy jobs for CI/CD
    
    * fix: adding in staging values to gitlab secrets
    
    * fix: consistent tagging and cleaner helm deploy command
    
    * fix: making master be a branch
    
    * feat: adding in deployment config validation
    
    * feat: adding better kubeconfig validation
    
    * fix: gitlab ci script format

--- build/build.sh
+++ build/build.sh
@@ -1,16 +1,28 @@
-#! /bin/sh
+#!/usr/bin/env bash
+#
+# Our helm charts have the version format 'x.y.z-a', where a is the number 
+# of commits since the last tagged version. A tagged version's 'a' will be 0.
+# CHART_VER is the 'x.y.z' part of the version. Helm charts may not have a v 
+# in their versioning tag. CHART_REL is the 'a' part of the version.
+#
+# The version and release is statically set if you cannot calculate it via git.
+# Additionally if CHART_VER or CHART_REL are set explicitly, those values will
+# have precendence over all.
 
-# Our helm charts have the version format 'x.y.z-a', where a is the number of commits since the last tagged version.
-# A tagged version's 'a' will be 0.
-# CHART_VER is the 'x.y.z' part of the version. Helm charts may not have a v in their versioning tag.
-# CHART_REL is the 'a' part of the version.
+export CHART_NAME=${CHART_NAME:-technical-on-boarding}
+export CHART_VER=${CHART_VER:-0.0.0}
+export CHART_REL=${CHART_REL:-0}
 
-CHART_VER=$(git describe --tags --abbrev=0 2>/dev/null | sed 's/^v//')
-if [ -n "${CHART_VER}" ]; then
-    CHART_REL=$(git rev-list --count v${CHART_VER}..HEAD 2>/dev/null )
-else
-    CHART_REL=$(git rev-list --count HEAD 2>/dev/null )
+# strip leading 'v' if present in version
+[[ ${CHART_VER} =~ ^v ]] && CHART_VER=${CHART_VER#v}
+
+# use git calculated version if you can AND if defaults are being used
+GIT_VER=$(git describe --tags --abbrev=0 2>/dev/null | sed 's/^v//')
+if [[ -n ${GIT_VER} && ${CHART_VER} = 0.0.0 && ${CHART_REL} = 0 ]]; then
+  CHART_VER=${GIT_VER}
+  CHART_REL=$(git rev-list --count v${CHART_VER}..HEAD 2>/dev/null)
 fi
 
-CHART_VER=${CHART_VER:-0.0.1} CHART_REL=${CHART_REL:-0} \
-    envsubst < build/Chart.yaml.in > ${CHART_NAME}/Chart.yaml
+# generate chart.yaml
+echo Using chart version: ${CHART_VER}-${CHART_REL}
+envsubst < build/Chart.yaml.in > ${CHART_NAME}/Chart.yaml
--- build/test.sh
+++ build/test.sh
@@ -1,15 +1,27 @@
-#! /bin/sh
+#!/usr/bin/env bash
+#
+# This script tests a helm chart is well-formed (helm lint) and tries to install 
+# and test a deployment via helm test (https://docs.helm.sh/developing_charts/#chart-tests)
 
-[[ -z "$CHART_NAME"   ]] && \
-  {
-    echo >&2 "Var '$CHART_NAME' is empty. Cannot continue."
-    exit 1
-  }
+CHART_NAME=${CHART_NAME:-technical-on-boarding}
+NAMESPACE=${NAMESPACE:?NAMESPACE must be set}
+RELEASE=${RELEASE:?RELEASE must be set}
 
-[[ ! -d "${CHART_NAME}" ]] && \
-  {
-    echo >&2 "Directory for chart '$CHART_NAME' does not exist."
-    exit 1
-  }
+if [[ ! -d ${CHART_NAME} ]]; then
+  echo >&2 "Directory for chart '$CHART_NAME' does not exist."
+  exit 1
+fi
 
-helm lint ${CHART_NAME}
+# test the format of the chart, exit on failure
+helm lint ${CHART_NAME} || exit $?
+
+# test the deployment of chart, and helm test if you got them
+helm install --wait --replace --name ${RELEASE} --namespace ${NAMESPACE} ./${CHART_NAME}
+if [[ -d ${CHART_NAME}/templates/tests ]]; then
+  helm test ${RELEASE} --cleanup
+  HELM_TEST_EXIT_CODE=$?
+fi
+
+# cleanup
+helm delete --purge ${RELEASE}
+exit ${HELM_TEST_EXIT_CODE}
